2c903349a4757c4d13267b1ead371a9a318fbd1c
ara-shun <aarraasshhuunn@gmail.com>
2025-07-01 19:23:45 +0900

    delete makecsv

--- definition/BME280_test.py
+++ definition/BME280_test.py
@@ -1,6 +1,6 @@
 import smbus
 import time
-import make_csv
+# import make_csv
 
 from bme280 import BME280Sensor
 
@@ -14,13 +14,17 @@ try:
             bme.read_data()
         except Exception as e:
             print(f"An error occurred during empty measurement in BME: {e}")
-            make_csv.print('msg', f"An error occurred during empty measurement in BME: {e}")
+            # make_csv.print('msg', f"An error occurred during empty measurement in BME: {e}")
 
     data = bme.read_data()  # ここでデータを取得
     pressure = bme.compensate_P(data)  # 気圧を補正して取得
-    make_csv.print("alt_base_press", pressure)
+    # make_csv.print("alt_base_press", pressure)
     baseline = bme.baseline(pressure)
 
 except Exception as e:
     print(f"An error occurred in setting bme object: {e}")
-    make_csv.print('serious_error', f"An error occurred in setting bme280 object: {e}")
+    # make_csv.print('serious_error', f"An error occurred in setting bme280 object: {e}")
+
+# ぼくは、、、、も、、う、、、、、、、、
+# ここにたどり着いた人へ。
+# ディスコードのアクティビティの表示には気を付けよう！
