a15db90e7e0918a81acc9b72d5f1f57debce5f16
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-07-14 11:04:23 +0900

    異常値調整したお

--- definition/ijochi.py
+++ definition/ijochi.py
@@ -1,6 +1,6 @@
 # センサーの値を補正する関数
 
-# 異常値範囲テーブル(合計値)
+# 異常値範囲テーブル(合計値) 
 abnormal_value_table = {
     "bme": {
         "temperature": {"min": 0, "max": 60},
@@ -8,35 +8,33 @@ abnormal_value_table = {
         "pressure": {"min": 800, "max": 1100},
     },
     "bno": {
-        "accel": {"min": -16, "max": 16},
-        "gyro": {"min": -2000, "max": 2000},
-        "mag": {"min": -1300, "max": 1300},
-        "liner_accel": {"min": 10, "max": 10},
-        "gravity": {"min": 1, "max": 1},
-        "temperature": {"min": -40, "max": 85},
+        "accel": {"min": 0, "max": 50},
+        "gyro": {"min": 0, "max": 45},
+        "mag": {"min": 0, "max": 250},
+        "liner_accel": {"min": 0, "max": 25},
+        "gravity": {"min": 0, "max": 15},
+        "temperature": {"min": 0, "max": 60},
     },
     "gps": {
-        "latitude": {"min": -90, "max": 90},
-        "longitude": {"min": -180, "max": 180},
-        "altitude": {"min": -500, "max": 10000},  # 標高（例：-500m～10000m）
-        "speed": {"min": 0, "max": 500},  # km/h（例：0～500km/h）
+        "latitude": {"min": 30, "max": 50},  # 日本の範囲内
+        "longitude": {"min": 130, "max": 150},
+        "altitude": {"min": -100, "max": 500},  # 測定可能高度は18000mまで
     },
     "distance_sensor": {
-        "distance": {"min": 0, "max": 10000},  # m（例：0～10m）
-        "signal_strength": {"min": 0, "max": 100},  # 信号強度（例：0～100%）
+        "distance": {"min": -2, "max": 100},  # m（例：0～10m）
     }
 }
 
 
 # 入力想定
-# 温度センサ：bme，9軸センサ：bno
-# 入力は数値(取得データ変数そのまま引数へ)
+# 温度センサ：bme，9軸センサ：bno，GPS：gps，超音波センサ：distance_sensor
+# 入力は数値(取得データ変数そのまま引数へ, scalingFactor適用済)
 def abnormal_check(sensor_name, value_name, sensor_value, ERROR_FLAG=True):
     try:
         # BNOはリストで帰ってくる
         if isinstance(sensor_value, list):
             # sensor_valueが全て0の場合は異常値
-            if all(v == 0 for v in sensor_value):
+            if all(v == 0 for v in sensor_value) and value_name != "gyro":
                 filtered_value = None
                 try:
                     if ERROR_FLAG:
