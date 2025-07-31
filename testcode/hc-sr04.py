e476f79ef9d311b389e72e5ae7b9ddd1e33952c2
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-07-31 15:37:36 +0900

    Update hc-sr04.py

--- testcode/hc-sr04.py
+++ testcode/hc-sr04.py
@@ -45,7 +45,8 @@ def measure_distance():
             return None  # タイムアウト
         
     # パルス幅から距離を計算
-    pulse_duration = pulse_end - pulse_start
+    if (pulse_start is None) or (pulse_end is None):
+        return None
     
     # 距離(cm) = (時間(s) * 音速(cm/s)) / 2
     distance = ((pulse_duration / 1000000.0) * sound_velosity) / 2
