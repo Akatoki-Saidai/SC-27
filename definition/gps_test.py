54bda75d665c457866f0344c16557344c28f75b0
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-08-07 19:21:53 +0900

    Update gps_test.py

--- definition/gps_test.py
+++ definition/gps_test.py
@@ -3,18 +3,18 @@ import gps
 # 使用例
 if __name__ == '__main__':
     print("緯度経度を取得中...")
-    latitude, longitude = idokeido()
+    latitude, longitude = gps.idokeido()
     if latitude is not None and longitude is not None:
         print(f"緯度: {latitude}, 経度: {longitude}")
     else:
         print("緯度経度の取得に失敗しました。")
 
     print("\n日本時間を取得中...")
-    japan_time = zikan()
+    japan_time = gps.zikan()
     if japan_time is not None:
         print(f"日本時間: {japan_time}")
         print("\n曜日を抽出中...")
-        weekday_result = youbi(japan_time)
+        weekday_result = gps.youbi(japan_time)
         if weekday_result is not None:
             print(f"曜日: {weekday_result}")
         else:
