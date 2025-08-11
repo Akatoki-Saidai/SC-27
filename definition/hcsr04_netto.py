91359358899a81b368119363af1926a659e04d79
ara-shun <aarraasshhuunn@gmail.com>
2025-08-11 19:52:50 +0900

    Update hcsr04_netto.py

--- definition/hcsr04_netto.py
+++ definition/hcsr04_netto.py
@@ -11,21 +11,25 @@ GPIO.setwarnings(False)                 # BPIO警告無効化
 GPIO.setup(trig_pin, GPIO.OUT)          # Trigピン出力モード設定
 GPIO.setup(echo_pin, GPIO.IN)           # Echoピン入力モード設定
 
-def get_distance(): 
-    #Trigピンを10μsだけHIGHにして超音波の発信開始
-    GPIO.output(trig_pin, GPIO.HIGH)
-    time.sleep(0.000010)
-    GPIO.output(trig_pin, GPIO.LOW)
+def get_distance():
+    # Trigピンを10μsだけHIGHにして超音波の発信開始
+    GPIO.output(trig_pin, GPIO.HIGH)
+    time.sleep(0.000010)
+    GPIO.output(trig_pin, GPIO.LOW)
 
-    while not GPIO.input(echo_pin):
-        pass
-    t1 = time.time() # 超音波発信時刻（EchoピンがHIGHになった時刻）格納
+    start_time = time.time()
+    while not GPIO.input(echo_pin):
+        if (time.time() - start_time) > 0.1: # 0.1秒経ってもHIGHにならない場合
+            return -1 # -1などエラー値を返す
+    t1 = time.time()
 
-    while GPIO.input(echo_pin):
-        pass
-    t2 = time.time() # 超音波受信時刻（EchoピンがLOWになった時刻）格納
+    start_time = time.time()
+    while GPIO.input(echo_pin):
+        if (time.time() - start_time) > 0.1: # 0.1秒経ってもLOWにならない場合
+            return -1 # -1などエラー値を返す
+    t2 = time.time()
 
-    return (t2 - t1) * speed_of_sound / 2 # 時間差から対象物までの距離計算
+    return (t2 - t1) * speed_of_sound / 2
 
 
 while True:                                         # 繰り返し処理
