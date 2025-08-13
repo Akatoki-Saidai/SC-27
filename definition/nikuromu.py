551b3c9a56e69944c51a8aaf054e887ab6de2e89
ara-shun <aarraasshhuunn@gmail.com>
2025-08-13 23:37:23 +0900

    Update nikuromu.py

--- definition/nikuromu.py
+++ definition/nikuromu.py
@@ -7,7 +7,7 @@ try:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(nichrome_pin, GPIO.OUT)
     GPIO.output(nichrome_pin, 1)
-    time.sleep(5) # 5秒あつくする
+    time.sleep(10) # 10秒あつくする
     GPIO.output(nichrome_pin, 0)
 
     print("ニクロム線切断完了")
