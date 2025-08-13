26827c33173aa3f9f78bab39568323e1aeb45c07
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-08-14 00:03:02 +0900

    Update nikuromu.py

--- definition/nikuromu.py
+++ definition/nikuromu.py
@@ -6,6 +6,10 @@ nichrome_pin = 16
 try:
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(nichrome_pin, GPIO.OUT)
+    GPIO.output(nichrome_pin, 0)
+    print("nicr off")
+    time.sleep(1)
+    print("nicr on")
     GPIO.output(nichrome_pin, 1)
     for i in range(1, 10 + 1):
         print(f"{i}秒目")
