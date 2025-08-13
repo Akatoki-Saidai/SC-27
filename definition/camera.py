d271761f349a5631991119b816e5652dcf30ef0f
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-08-13 21:47:28 +0900

    こっちにもコピペ

--- definition/camera.py
+++ definition/camera.py
@@ -1,5 +1,4 @@
 # camera
-import os
 import time
 from ultralytics import YOLO
 import cv2
@@ -9,13 +8,24 @@ from picamera2 import Picamera2
 # 同じディレクトリに重みを置く
 pt_path = "./my_custom_model.pt"
 
-
+# YOLOv11nモデルをロード
+try:
+    model = YOLO(pt_path)
+    print("YOLO model loaded.")
+except Exception as e:
+    print(f"Error loading YOLO model: {e}")
+    model = None
+    
 def yolo_detect(frame):
+    global model
+    
     yolo_xylist = 0
     center_x = 0
     
-    # YOLOv10nモデルをロード
-    model = YOLO(pt_path)
+    if model is None:
+        print("YOLO model reloaded.")
+        model = YOLO(pt_path)
+
     # 推論
     yolo_results = model.predict(frame, save = False, show = False)
     print(type(yolo_results))
@@ -37,17 +47,15 @@ def yolo_detect(frame):
         center_x = 0
 
     else:
-        for i in range(len(Bounding_box)):
-            confidence = confidences[i]
-            if confidence < confidence_best:
-                continue
-            else:
-                confidence_best = confidence
-            xmin, ymin, xmax, ymax = Bounding_box[i]
-                
+        best_index = np.argmax(confidences)
+        confidence_best = confidences[best_index]
+        best_box = Bounding_box[best_index]
+        
+        xmin, ymin, xmax, ymax = best_box
+        
         center_x = int(xmin + (xmax - xmin) / 2)
-        yolo_xylist = [xmin, ymin, xmax, ymax, confidence]
-
+        yolo_xylist = [xmin, ymin, xmax, ymax, confidence_best]
+    
     return yolo_xylist, center_x
 
 
@@ -55,9 +63,9 @@ def red_detect(frame):
     # HSV色空間に変換
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # 赤色のHSVの値域1
-    hsv_min = np.array([0, 117, 104])
-    hsv_max = np.array([11, 255, 255])
+    # 赤色のHSVの値域1  (カメラの都合でかなりオレンジ寄りです)
+    hsv_min = np.array([0, 117, 115])  # 元の値[0, 117, 104]
+    hsv_max = np.array([18, 255, 255])  # 元の値[11, 255, 255]
     mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
 
     # 赤色のHSVの値域2
@@ -113,7 +121,7 @@ def judge_cone(frame):
             # print(f"red area: {red_area}")
             # フレーム内の赤の割合を計算
             red_persent = red_area / (frame.shape[0] * frame.shape[1])
-            print(f"red persent: {red_persent} %")
+            print(f"red persent: {red_persent * 100:.2f} %")
 
         except Exception as e:
             print(f"An error occured in analize_red: {e}")
@@ -121,59 +129,75 @@ def judge_cone(frame):
 
         colorcone_x_persent = 0
         # 中心座標のx座標が画像の中心より大きいか小さいか判定→超音波へ
-        if red_persent > 0.6:
+        if red_persent > 0.3: # 30 %以上（めっちゃ近い）
             print("Close to red, check distance of ultrasonic")
             camera_order = 4
 
-        elif red_persent > 0.4:
+        elif red_persent > 0.1: # 10 %以上（ちょい近い）
             print("judge red object by color")
 
             # 画像幅に対してどのくらい離れているか計算
             colorcone_x_persent = (red_center_x - frame_center_x) / frame.shape[1]
             print(f"relative_red_x: {colorcone_x_persent}")
 
-            if -0.05 <= colorcone_x_persent <= 0.055:
+            if -0.25 <= colorcone_x_persent <= 0.25:
                 print("The red object is in the center")  #直進
                 camera_order = 1
-            elif colorcone_x_persent > 0.05:
+            elif colorcone_x_persent > 0.25:
                 print("The red object is in the right")  #右へ
                 camera_order = 2
-            elif colorcone_x_persent < -0.05:
+            elif colorcone_x_persent < -0.25:
                 print("The red object is in the left")  #左へ
                 camera_order = 3
             else:
                 print("The red object is too minimum")
                 camera_order = 0
 
-        elif red_persent > 0.02:
-            print("judge red object by yolo")
-
+        elif red_persent > 0.01: # 1 %以上（遠すぎ or 無さそう）
             try:
+                print("judge red object by yolo")
                 # YOLO呼び出し
                 yolo_xylist, yolo_center_x = yolo_detect(frame)
                 print(f"yolo_xylist: {yolo_xylist}, yolo_center_x: {yolo_center_x}")
+
+                if yolo_xylist:
+                    colorcone_x_persent = (yolo_center_x - frame_center_x) / frame.shape[1]
+                    print(f"relative_yolo_x: {colorcone_x_persent}")
+                    
+                    if -0.25 <= colorcone_x_persent <= 0.25:
+                        print("The yolo object is in the center")
+                        camera_order = 1
+                    elif colorcone_x_persent > 0.25:
+                        print("The yolo object is in the right")
+                        camera_order = 2
+                    elif colorcone_x_persent < -0.25:
+                        print("The yolo object is in the left")
+                        camera_order = 3
+                else:
+                    # YOLOが何も検出しなかった場合
+                    print("The yolo object is not found")
+                    camera_order = 0
+
             except Exception as e:
                 print(f"An error occured in yolo_detect: {e}")
-
-            # 画像幅に対してどのくらい離れているか計算
-            colorcone_x_persent = (red_center_x - frame_center_x) / (frame.shape[0] * frame.shape[1])
-            print(f"relative_red_x: {colorcone_x_persent}")
-            
-            if -0.05 <= colorcone_x_persent <= 0.05:
-                print("The yolo object is in the center")  #直進
-                camera_order = 1
-            elif colorcone_x_persent > 0.05:
-                print("The yolo object is in the right")  #右へ
-                camera_order = 2
-            elif colorcone_x_persent < -0.05:
-                print("The yolo object is in the left")  #左へ
-                camera_order = 3
-            else:
-                print("The yolo object is too minimum")
                 camera_order = 0
         
         else:
-            print("Colorcone is None")
+            print("Colorcone is None or too small")
+            camera_order = 0
+
+            # どこにあるかだけ赤色検出
+            colorcone_x_persent = (red_center_x - frame_center_x) / frame.shape[1]
+            print(f"relative_red_x: {colorcone_x_persent}")
+            if -0.25 <= colorcone_x_persent <= 0.25:
+                print("Red is in the center")
+            elif colorcone_x_persent > 0.25:
+                print("Red is in the right")
+            elif colorcone_x_persent < -0.25:
+                print("Red is in the left")
+            else:
+                print("Red is none")
+            
         
         if yolo_xylist:
             # Bounding Box描画
@@ -181,7 +205,7 @@ def judge_cone(frame):
             cv2.putText(frame, str(yolo_xylist[4]), (int(yolo_xylist[0]), int(yolo_xylist[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0))
 
             # 面積表示
-            cv2.putText(frame, f"{red_persent} %", (int(yolo_xylist[0]), int(yolo_xylist[3] - 5)), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (255, 255, 255))
+            cv2.putText(frame, f"{red_persent * 100:.2f} %", (int(yolo_xylist[0]), int(yolo_xylist[3] - 5)), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (255, 255, 255))
 
             # red_result = cv2.drawContours(mask, [biggest_contour], -1, (0, 255, 0), 2)
         
@@ -203,7 +227,12 @@ if __name__ == '__main__':
         picam2 = Picamera2()
         config = picam2.create_preview_configuration({"format": 'XRGB8888', "size": (1280, 720)})
         picam2.configure(config)  # カメラの初期設定
-        
+        picam2.set_controls({
+            "ExposureTime": 10000,  # 露光時間
+            "AnalogueGain": 1.0,  # ゲイン
+            # "AwbEnable": False,  # ホワイトバランス
+            # "ColourGains": (1.5, 1.2)  # (赤, 青)
+        })
         picam2.start()
 
         while True:
