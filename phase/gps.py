40579c704c050f4a3277ef6887b7fbd5556f9c68
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-08-15 03:41:58 +0900

    もうGPSv2
    結構きれいなのでは

--- phase/gps.py
+++ phase/gps.py
@@ -8,13 +8,7 @@ import math
 port = "/dev/serial0"
 baudrate = 38400
 
-# WGS84楕円体のパラメータを定義
-a = 6378137.0
-b = 6356752.314245
-f = (a - b) / a
-
-# 緯度経度をWGS84楕円体に基づいて設定
-goal_lat, goal_lon = 40.14389563045866, 139.98732883121738 # 能代宇宙広場 (ゴール地点の例)
+goal_lat, goal_lon = 40.14389563045866, 139.98732883121738 # 能代宇宙広場 (ゴール地点)
 
 # pyprojを使ってWGS84楕円体に基づく投影を定義
 wgs84 = pyproj.CRS('EPSG:4326')
@@ -22,44 +16,114 @@ utm = pyproj.CRS('+proj=utm +zone=54 +ellps=WGS84')
 transformer = pyproj.Transformer.from_crs(wgs84, utm, always_xy=True)
 
 
-def zenbu():
-    try:
-        with serial.Serial(port, baudrate, timeout=1) as ser:
-            start_time = time.time()
-            while (time.time() - start_time) < 15:  # 15秒間試行
-                line = ser.readline().decode('ascii', errors='replace')
-                print(f"line: {line}")
-    except serial.SerialException as e:
-        print(f"Serial error: {e}")
-        return None
+class GpsManager:
+    def __init__(self, port, baudrate):
+        self.port = port
+        self.baudrate = baudrate
+        self.ser = None
+        self.latitude = None
+        self.longitude = None
+        self.timestamp_jst = None
+        
+        # UART開始
+        if self.ser is None or not self.ser.is_open:
+            try:
+                self.ser = serial.Serial(self.port, self.baudrate, timeout=1)
+                print("Successfully connected to serial port.")
+            except serial.SerialException as e:
+                print(f"Failed to connect to serial port: {e}")
+                self.ser = None
 
-def idokeido():
-    """
-    緯度と経度を抽出します
-    一定時間（10秒）GPSデータが取得できなかったらNoneを返します
-    """
-    try:
-        with serial.Serial(port, baudrate, timeout=1) as ser:
-            start_time = time.time()
-            while (time.time() - start_time) < 10:  # 10秒間試行
-                line = ser.readline().decode('ascii', errors='replace')
-                # GGA, RMC, GLLで衛星システム(GP,GL,GA,GN等)を問わず処理
-                if line.startswith('$') and (line[3:6] == 'GGA' or line[3:6] == 'RMC' or line[3:6] == 'GLL'):
+    def update(self):
+        # UARTバッファを全て読み込み、最新に更新
+        if not self.ser:
+            try:
+                self.ser = serial.Serial(self.port, self.baudrate, timeout=1)
+                print("Successfully reconnected to serial port.")
+            except serial.SerialException as e:
+                print(f"Failed to connect to serial port: {e}")
+                self.ser = None
+            if not self.ser:
+                return
+
+        try:
+            # バッファのデータ全て読み込み
+            if self.ser.in_waiting > 0:
+                buffer = self.ser.read(self.ser.in_waiting).decode('ascii', errors='replace')
+                line_list = buffer.split('\n')
+
+                temp_lat = None
+                temp_lon = None
+                temp_dt_jst = None
+                
+                for line in line_list:
                     try:
                         msg = pynmea2.parse(line)
+
                         # 有効な緯度経度データかチェック
                         if hasattr(msg, 'latitude') and hasattr(msg, 'longitude') and msg.latitude != 0.0:
-                            lat = msg.latitude
-                            lon = msg.longitude
-                            return lat, lon
+                            temp_lat = msg.latitude
+                            temp_lon = msg.longitude
+                        
+                        # 日付と時刻を含むRMCセンテンスから情報を更新
+                        if isinstance(msg, pynmea2.types.talker.RMC):
+                            if msg.datestamp and msg.timestamp:
+                                dt_utc = datetime.combine(msg.datestamp, msg.timestamp)
+                                temp_dt_jst = dt_utc + timedelta(hours=9)
                     except pynmea2.ParseError:
                         continue
-            print("idokeido: 10秒以内にGPSデータが取得できませんでした。")
-            return None, None
-    except serial.SerialException as e:
-        print(f"Serial error: {e}")
-        return None, None
+                    except Exception as e:
+                        print(f"An error occured in parse uart line")
+                
+                if temp_lat is not None:
+                    self.latitude = temp_lat
+                    self.longitude = temp_lon
+                if temp_dt_jst is not None:
+                    self.timestamp_jst = temp_dt_jst.strftime('%Y-%m-%d %H:%M:%S')
+                    
+        except serial.SerialException as e:
+            print(f"Error during serial read: {e}")
+            self.ser.close()
+            self.ser = None
 
+      
+gps_manager = GpsManager(port, baudrate)
+
+def idokeido():
+    # 緯度と経度を抽出、内部でGPSデータを最新に更新
+    gps_manager.update()
+    latitude = gps_manager.latitude
+    longitude = gps_manager.longitude
+
+    # データが一度も取得できていない場合はNoneを返す
+    if latitude is None or longitude is None:
+        print("idokeido: 有効なGPSデータが取得できませんでした")
+        return None, None
+    return latitude, longitude
+
+def zikan():
+    # 日本時間を抽出、内部でGPSデータを最新に更新
+    gps_manager.update()
+    time_jst = gps_manager.timestamp_jst
+    
+    # データが一度も取得できていない場合はNoneを返す
+    if time_jst is None:
+        print("zikan: 有効なGPSデータが取得できませんでした")
+        return None
+    return time_jst
+
+def youbi(datetime_str):
+    # 曜日を抽出します
+    try:
+        # 文字列を datetime オブジェクトに変換
+        dt_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+        weekday = dt_object.strftime('%A')
+        return weekday
+    except ValueError:
+        print(f"エラー: 無効な日時文字列のフォーマットです: {datetime_str}")
+        return None
+
+# 余弦定理でゴール角度を計算
 def calculate_distance_and_angle(current_lat, current_lon, start_lat, start_lon, goal_lat, goal_lon):
     # 現在地の緯度経度をメートルに変換
     current_x, current_y = transformer.transform(current_lon, current_lat)
@@ -85,42 +149,4 @@ def calculate_distance_and_angle(current_lat, current_lon, start_lat, start_lon,
         return distance_start_goal, theta_for_goal
     except Exception as e:
         print(f"移動していません: {e}")
-        return 2323232323, math.pi * 2
-
-def zikan():
-    """
-    日本時間を抽出します
-    一定時間（15秒）GPSデータが取得できなかったらNoneを返します
-    """
-    try:
-        with serial.Serial(port, baudrate, timeout=1) as ser:
-            start_time = time.time()
-            while (time.time() - start_time) < 15:  # 15秒間試行
-                line = ser.readline().decode('ascii', errors='replace')
-                if line.startswith('$') and line[3:6] == 'RMC':
-                    try:
-                        msg = pynmea2.parse(line)
-                        if msg.datestamp and msg.timestamp:
-                            dt_utc = datetime.combine(msg.datestamp, msg.timestamp)
-                            dt_jst = dt_utc + timedelta(hours=9)
-                            return dt_jst.strftime('%Y-%m-%d %H:%M:%S')
-                    except pynmea2.ParseError:
-                        continue
-            print("zikan: 15秒以内にGPSデータが取得できませんでした。")
-            return None
-    except serial.SerialException as e:
-        print(f"Serial error: {e}")
-        return None
-
-def youbi(datetime_str):
-    """
-    曜日を抽出します
-    """
-    try:
-        # 文字列を datetime オブジェクトに変換
-        dt_object = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
-        weekday = dt_object.strftime('%A')
-        return weekday
-    except ValueError:
-        print(f"エラー: 無効な日時文字列のフォーマットです: {datetime_str}")
-        return None
+        return 2727272727, math.pi * 2
