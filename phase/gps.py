1d420a7bc1028a7ed0b20b0f4a717220895a3ae5
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-08-15 20:48:35 +0900

    GPSmanagerをグローバルに
    今のところ綺麗な動かないコード(´;ω;｀)

--- phase/gps.py
+++ phase/gps.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 import pyproj
 import math
 
-port = "/dev/serial0"
-baudrate = 38400
+PORT = "/dev/serial0"
+BAUDRATE = 38400
 
 goal_lat, goal_lon = 40.142661833333335, 139.9876495 # 能代宇宙広場 (ゴール地点)
 
@@ -51,6 +51,7 @@ class GpsManager:
             if self.ser.in_waiting > 0:
                 buffer = self.ser.read(self.ser.in_waiting).decode('ascii', errors='replace')
                 line_list = buffer.split('\n')
+                print(f"line_list: {line_list}")
 
                 temp_lat = None
                 temp_lon = None
@@ -58,6 +59,7 @@ class GpsManager:
                 
                 for line in line_list:
                     try:
+                        print(f"NMEA: {line.strip()}")
                         msg = pynmea2.parse(line)
 
                         # 有効な緯度経度データかチェック
@@ -87,9 +89,11 @@ class GpsManager:
             self.ser = None
 
       
-gps_manager = GpsManager(port, baudrate)
+gps_manager = GpsManager(PORT, BAUDRATE)
 
 def idokeido():
+    global gps_manager
+
     # 緯度と経度を抽出、内部でGPSデータを最新に更新
     gps_manager.update()
     latitude = gps_manager.latitude
@@ -102,6 +106,8 @@ def idokeido():
     return latitude, longitude
 
 def zikan():
+    global gps_manager
+
     # 日本時間を抽出、内部でGPSデータを最新に更新
     gps_manager.update()
     time_jst = gps_manager.timestamp_jst
@@ -150,3 +156,5 @@ def calculate_distance_and_angle(current_lat, current_lon, start_lat, start_lon,
     except Exception as e:
         print(f"移動していません: {e}")
         return 2727272727, math.pi * 2
+
+
